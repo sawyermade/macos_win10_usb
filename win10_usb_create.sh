a798c22ac8135f4fbc82370e7687b07db40a9b46
sawyermade <sawyermade0@gmail.com>
2022-07-20 16:26:38 -0400

    bash

--- win10_usb_create.sh
+++ win10_usb_create.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+DEBUG=true
+
+# Get USB disk number
+diskutil list
+read -p "Enter disk number. Example: 4: " DISKNUM
+if [ $DEBUG == true ]; then echo $DISKNUM; fi;
+
 # Mount Windows 10 ISO and save mount point as $ISOMOUNT
 read -e -p "Path to Windows 10 ISO: " ISOPATH
 ISOPATH="${ISOPATH/"~"/$HOME}"
 ISOVAR=$(hdiutil mount ${ISOPATH})
 ISOMOUNT=/Volumes/${ISOVAR##*/}
-echo $ISOMOUNT
+if [ $DEBUG == true ]; then echo $ISOMOUNT; fi;
